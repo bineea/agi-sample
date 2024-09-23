cf330625ef4dd01e2141278c775f07870424afe1
guowb1 <guowb1@lenovo.com>
2024-09-23 22:40:30 +0800

    分层代理

---
+++ src/agisample/framework/graph/hierarchical_agent_teams/handle_agent.py
@@ -0,0 +1,126 @@
+from typing import List, Optional, Annotated, TypedDict
+
+from langchain.agents import AgentExecutor, create_openai_functions_agent
+from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+
+from langgraph.graph import END, StateGraph, START
+
+from agisample.framework.graph.hierarchical_agent_teams.web_tool import scrape_webpages, tavily_tool
+
+
+def create_agent(
+    llm: ChatOpenAI,
+    tools: list,
+    system_prompt: str,
+) -> str:
+    """Create a function-calling agent and add it to the graph."""
+    system_prompt += "\nWork autonomously according to your specialty, using the tools available to you."
+    " Do not ask for clarification."
+    " Your other team members (and other teams) will collaborate with you with their own specialties."
+    " You are chosen for a reason! You are one of the following team members: {team_members}."
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                system_prompt,
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+    )
+    agent = create_openai_functions_agent(llm, tools, prompt)
+    executor = AgentExecutor(agent=agent, tools=tools)
+    return executor
+
+
+def agent_node(state, agent, name):
+    result = agent.invoke(state)
+    return {"messages": [HumanMessage(content=result["output"], name=name)]}
+
+
+def create_team_supervisor(llm: ChatOpenAI, system_prompt, members) -> str:
+    """An LLM-based router."""
+    options = ["FINISH"] + members
+    function_def = {
+        "name": "route",
+        "description": "Select the next role.",
+        "parameters": {
+            "title": "routeSchema",
+            "type": "object",
+            "properties": {
+                "next": {
+                    "title": "Next",
+                    "anyOf": [
+                        {"enum": options},
+                    ],
+                },
+            },
+            "required": ["next"],
+        },
+    }
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder(variable_name="messages"),
+            (
+                "system",
+                "Given the conversation above, who should act next?"
+                " Or should we FINISH? Select one of: {options}",
+            ),
+        ]
+    ).partial(options=str(options), team_members=", ".join(members))
+    return (
+        prompt
+        | llm.bind_functions(functions=[function_def], function_call="route")
+        | JsonOutputFunctionsParser()
+    )
+
+
+import functools
+import operator
+
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai.chat_models import ChatOpenAI
+
+
+# ResearchTeam graph state
+class ResearchTeamState(TypedDict):
+    # A message is added after each team member finishes
+    messages: Annotated[List[BaseMessage], operator.add]
+    # The team members are tracked so they are aware of
+    # the others' skill-sets
+    team_members: List[str]
+    # Used to route work. The supervisor calls a function
+    # that will update this every time it makes a decision
+    next: str
+
+
+llm = ChatOpenAI(model="gpt-4-1106-preview")
+
+search_agent = create_agent(
+    llm,
+    [tavily_tool],
+    "You are a research assistant who can search for up-to-date info using the tavily search engine.",
+)
+search_node = functools.partial(agent_node, agent=search_agent, name="Search")
+
+research_agent = create_agent(
+    llm,
+    [scrape_webpages],
+    "You are a research assistant who can scrape specified urls for more detailed information using the scrape_webpages function.",
+)
+research_node = functools.partial(agent_node, agent=research_agent, name="WebScraper")
+
+supervisor_agent = create_team_supervisor(
+    llm,
+    "You are a supervisor tasked with managing a conversation between the"
+    " following workers:  Search, WebScraper. Given the following user request,"
+    " respond with the worker to act next. Each worker will perform a"
+    " task and respond with their results and status. When finished,"
+    " respond with FINISH.",
+    ["Search", "WebScraper"],
+)
+
+
---
+++ src/agisample/framework/graph/hierarchical_agent_teams/web_team.py
@@ -0,0 +1,82 @@
+import functools
+import operator
+from typing import List, Annotated, TypedDict
+
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_openai.chat_models import ChatOpenAI
+from langgraph.constants import START, END
+from langgraph.graph import StateGraph
+
+from agisample.framework.graph.hierarchical_agent_teams.handle_agent import create_team_supervisor, agent_node, \
+    create_agent
+from agisample.framework.graph.hierarchical_agent_teams.web_tool import scrape_webpages, tavily_tool
+
+
+# ResearchTeam graph state
+class ResearchTeamState(TypedDict):
+    # A message is added after each team member finishes
+    messages: Annotated[List[BaseMessage], operator.add]
+    # The team members are tracked so they are aware of
+    # the others' skill-sets
+    team_members: List[str]
+    # Used to route work. The supervisor calls a function
+    # that will update this every time it makes a decision
+    next: str
+
+
+llm = ChatOpenAI(model="gpt-4-1106-preview")
+
+search_agent = create_agent(
+    llm,
+    [tavily_tool],
+    "You are a research assistant who can search for up-to-date info using the tavily search engine.",
+)
+search_node = functools.partial(agent_node, agent=search_agent, name="Search")
+
+research_agent = create_agent(
+    llm,
+    [scrape_webpages],
+    "You are a research assistant who can scrape specified urls for more detailed information using the scrape_webpages function.",
+)
+research_node = functools.partial(agent_node, agent=research_agent, name="WebScraper")
+
+supervisor_agent = create_team_supervisor(
+    llm,
+    "You are a supervisor tasked with managing a conversation between the"
+    " following workers:  Search, WebScraper. Given the following user request,"
+    " respond with the worker to act next. Each worker will perform a"
+    " task and respond with their results and status. When finished,"
+    " respond with FINISH.",
+    ["Search", "WebScraper"],
+)
+
+research_graph = StateGraph(ResearchTeamState)
+research_graph.add_node("Search", search_node)
+research_graph.add_node("WebScraper", research_node)
+research_graph.add_node("supervisor", supervisor_agent)
+
+# Define the control flow
+research_graph.add_edge("Search", "supervisor")
+research_graph.add_edge("WebScraper", "supervisor")
+research_graph.add_conditional_edges(
+    "supervisor",
+    lambda x: x["next"],
+    {"Search": "Search", "WebScraper": "WebScraper", "FINISH": END},
+)
+
+
+research_graph.add_edge(START, "supervisor")
+chain = research_graph.compile()
+
+
+# The following functions interoperate between the top level graph state
+# and the state of the research sub-graph
+# this makes it so that the states of each graph don't get intermixed
+def enter_chain(message: str):
+    results = {
+        "messages": [HumanMessage(content=message)],
+    }
+    return results
+
+
+research_chain = enter_chain | chain
